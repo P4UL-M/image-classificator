e86b8d303752d8da5f4869de42b25e6aaae90c57
P4UL-M <paul.mairesse@free.fr>
2024-10-20 01:39:15 +0200

    Docker compose test and migration

--- Api/seeders/20241019223941-demo-user.js
+++ Api/seeders/20241019223941-demo-user.js
@@ -1,19 +1,20 @@
 'use strict';
-
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [{
       username: 'user1',
       email: 'user1@email.com',
-      password: 'password1',
+      password: bcrypt.hashSync('password1', process.env.SALT_ROUNDS || 10),
       balance: 100,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: 'user2',
       email: 'user2@email.com',
-      password: 'password2',
+      password: bcrypt.hashSync('password2', process.env.SALT_ROUNDS || 10),
       balance: 200,
       createdAt: new Date(),
       updatedAt: new Date()
